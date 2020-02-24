15bae7dbd2751a28e66ce049de94f513d2f10e01
ngk <ngkan146@gmail.com>
2020-02-24 20:28:24 +0100

    first commit

---
+++ vpp.cpp
@@ -0,0 +1,53 @@
+#include <bits/stdc++.h>
+#define va 			and
+#define tudong 		auto
+#define gay 		break
+#define truonghop	case
+#define bat			catch
+#define chu			char
+#define loai		class
+#define hangso		const
+#define	tieptuc		continue
+#define lam			do
+#define doi			double
+#define macdinh		default
+#define khongthi	else
+#define sai			false
+#define noi			float
+#define cho			for
+#define diden		goto
+#define neu			if
+#define nguyen		int		 
+#define dai			long
+#define chinh		main
+#define khonggian	namespace
+#define moi 		new
+#define khong		not
+#define phep		operator
+#define hoac		or
+#define rieng		private		
+#define congkhai	public
+#define trave		return
+#define ngan		short
+#define codau		signed
+#define	doloncua	sizeof
+#define tinh		static
+#define cautruc		struct
+#define chuyensang	switch
+#define mau			template
+#define cainay		this
+#define nem			throw
+#define dung		true
+#define thu			try
+#define dnkieu		typedef
+#define unsigned	khongdau
+#define sudung		using
+#define huvo		void
+#define trongkhi	while
+
+
+sudung khonggian std;
+
+nguyen chinh(){
+	nguyen x = 0;
+}
